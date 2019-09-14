b992670e7e56bac05b8b6b34ec5f8106a1d2c25b
Marc Boanas <marcjohnboanas@gmail.com>
2019-09-14 18:45:45 +0100

    Initial Commit

---
+++ Taxable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Taxable
+//
+//  Created by Marc Boanas on 14/09/2019.
+//  Copyright © 2019 Marc Boanas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
